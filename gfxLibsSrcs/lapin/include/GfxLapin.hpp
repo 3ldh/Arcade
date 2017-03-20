2b98dbf8920eebe7238595847f24537ed8a7e09f
Clement Peau <clement.peau@epitech.eu>
2017-03-20 17:02:56 +0100

    added support for liblapin

---
+++ gfxLibsSrcs/lapin/include/GfxLapin.hpp
@@ -0,0 +1,8 @@
+//
+// Created by peau_c on 3/20/17.
+//
+
+#ifndef CPP_ARCADE_GFXLAPIN_H
+#define CPP_ARCADE_GFXLAPIN_H
+
+#endif //CPP_ARCADE_GFXLAPIN_H
---
+++ gfxLibsSrcs/lapin/src/GfxLapin.cpp
@@ -0,0 +1,5 @@
+//
+// Created by peau_c on 3/20/17.
+//
+
+#include "../include/GfxLapin.hpp"
